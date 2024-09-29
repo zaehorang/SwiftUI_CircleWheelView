79d89eba2d7f6ffc3059a31cf558af472dba738c
zaehorang <zaehorang2@naver.com>
2024-09-29 18:24:55 +0900

    chore: Add new project

---
+++ SwiftUI_WheelView/SwiftUI_WheelView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_WheelView
+//
+//  Created by zaehorang on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_WheelView/SwiftUI_WheelView/SwiftUI_WheelViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WheelViewApp.swift
+//  SwiftUI_WheelView
+//
+//  Created by zaehorang on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WheelViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
